5b900c51a6440f236ec9cedda86f505fa055faba
AV-NASH <avinash21213434@gmail.com>
2020-10-08 22:24:57 +0530

    refactor:code refactored to include default and parameterized constructor

--- src/main/java/com/cg/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/cg/moodanalyser/MoodAnalyser.java
@@ -1,10 +1,19 @@
 package com.cg.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser() {
+        message="I am in Happy Mood";
+    }
+    public MoodAnalyser(String message) {
+        this.message=message;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to mood analyzer program\n");
     }
-    public String analyzeMood(String message) {
+    public String analyzeMood() {
         if(message.contains("Sad"))
             return "SAD";
         else return "HAPPY";
--- src/test/java/com/cg/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/cg/moodanalyser/MoodAnalyserTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.*;
 public class MoodAnalyserTest {
     @Test
     public void returnsSAD_whenmessage_ContainsSad() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        assertEquals("SAD",moodAnalyser.analyzeMood("I am in Sad Mood"));
+        MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Sad Mood");
+        assertEquals("SAD",moodAnalyser.analyzeMood());
     }
 
     @Test
     public void returnsHappy_whenmessage_NotContainsSad() {
         MoodAnalyser moodAnalyser=new MoodAnalyser();
-        assertEquals("HAPPY",moodAnalyser.analyzeMood("I am in Any Mood"));
+        assertEquals("HAPPY",moodAnalyser.analyzeMood());
     }
 }
